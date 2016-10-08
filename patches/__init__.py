f348011c0282f439267b2a39434b0e3363c92c54
Tony Narlock <tony@git-pull.com>
2016-10-08 13:52:59 -0500

    test add initial test, module

---
+++ patches/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# flake8: NOQA
+"""Portfolio management for open source contributors.
+
+patches
+~~~~~~~
+
+:copyright: Copyright 2016 Tony Narlock.
+:license: BSD, see LICENSE for details
+
+"""
+
---
+++ tests/test_patches.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+
+def test_this():
+    assert 1 == 1
